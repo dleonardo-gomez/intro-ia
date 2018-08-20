680971394b961c89ce14e250f306abf5c4df7ed1
Daniel Hamilton Smith <danihas@live.com>
2018-08-19 20:34:33 -0500

    separacion en archivos dependiendo de la funcion

---
+++ prolog/PrimeroListaHeu.pl
@@ -0,0 +1,55 @@
+
+
+/*
+%saca el mayor valor de una lista, tuve que copiar un codigo casi completamente   para lograr entender esto....
+mayor(Ccab, [],Ccab) .
+mayor(Ccab, [Cab|L],May ):- Ccab > Cab, mayor(Ccab,L,May) .
+mayor(Ccab, [Cab|L],May ):- Ccab =< Cab, mayor(Cab,L,May) .
+mayor([Cab|L],May) :-mayor(Cab,L,May) .
+*/
+
+
+%saca el mayor valor de una lista
+mayor(Ccab, [],Ccab) .
+
+mayor(Ccab, [Cab|L],May ):-
+    heu(Ccab,Cab,Val1,Val2),
+    Val1 > Val2,
+    mayor(Ccab,L,May).
+
+mayor(Ccab, [Cab|L],May ):-
+    heu(Ccab,Cab,Val1,Val2),
+    Val1 =< Val2,
+    mayor(Cab,L,May).
+
+mayor([Cab|L],May) :-
+    mayor(Cab,L,May).
+%-----------------------------
+
+heu(C1,C2,V1,V2):- heuri(C1,V1),heuri(C2,V2).
+
+% saca el tamaño de la lista Prioridades, lo guarda en Ubi y lo pasa
+% a listPri, que ira reduciendo el valor hasta llegar al punto en el
+% que este ubicado el valor, retornandolo por Val
+heuri(CC,Val):-
+    listPri(CC ,Val).
+
+%caso final no encontrado
+listPri(_,[],0).
+
+%caso final encontrado
+listPri(CC,[CC|L],Val):-
+    length(L,Vall), Val is Vall + 1.
+
+%%caso intermedio
+listPri(CC,[_|L],Val):-
+    listPri(CC,L,Val).
+
+% la lista de prioridades, con que mas al frente este el dato, mayor
+% valor tiene
+prioridades(Pp) :- Pp = [4,5,3,6].
+
+%llamada inicial
+listPri(CC,Val):-
+    prioridades(Pp),
+    listPri(CC,Pp,Val).
